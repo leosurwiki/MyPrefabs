5ec9e4c4a2fed6d314a8c0c7d023f589d133bd2c
leosurwiki <396568314@qq.com>
2016-04-04 16:21:29 -0700

    add swichableFixture

---
+++ scripts/PcSwitchedFixture.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PcSwitchedFixture : MonoBehaviour {
+    [Tooltip("The activation")]
+    public Pcswitch mypcswitch;
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        LPFixture[] lpfs = GetComponents<LPFixture>();
+        if (mypcswitch.getState())
+        {
+            if (sr.color != Color.gray)
+            {
+                foreach (LPFixture lpf in lpfs)
+                    lpf.Disable();
+            }
+            sr.color = Color.gray;
+        }
+	}
+}
