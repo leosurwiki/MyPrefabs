ba5a775514c90d580d638cac7511916c7af6bfa7
leosurwiki <396568314@qq.com>
2016-04-20 10:43:44 -0700

    color difference

--- scripts/PcWinCounter.cs
+++ scripts/PcWinCounter.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PcWinCounter : PcPartileFilter{
+public class PcWinCounter : PcPartileFilter
+{
     public LPFixtureBox box;
     public Texture2D celeste;
     public Texture2D blue;
-	public int threshold = 15;
+    public int threshold = 15;
     //override
     float areaLeftTopX;
     float areaLeftTopY;
@@ -23,22 +24,27 @@ public class PcWinCounter : PcPartileFilter{
     {
         ParticleNumber = 0;
     }
-    override public void  execute()
+    override public void execute()
     {
-		if (ParticleNumber > threshold)
+        if (ParticleNumber > threshold)
         {
-			int nextscene = 0;
-			if (nextscene > Application.levelCount - 1) {
-				nextscene = 0;
-				Application.LoadLevel(nextscene);
-			} else {
-				nextscene = Application.loadedLevel + 1;
-				string name = "level" + nextscene;
-				Debug.Log (name);
-				LevelSystem.SetLevels (name,true);
-				Application.LoadLevel(nextscene);
-			}
-		
+            int nextscene = Application.loadedLevel + 1;
+            Debug.Log("nextScene " + nextscene);
+            if (nextscene >= Application.levelCount - 2)
+            {//TODO
+                nextscene = 0;
+                Application.LoadLevel(nextscene);
+            }
+            else
+            {
+                //	nextscene = Application.loadedLevel + 1;
+                string name = "level" + nextscene;
+                Debug.Log(name);
+                LevelSystem.SetLevels(name, true);
+                Debug.Log("update level " + nextscene);
+                Application.LoadLevel(nextscene);
+            }
+
         }
     }
     //override
@@ -51,6 +57,6 @@ public class PcWinCounter : PcPartileFilter{
     void OnGUI()
     {
         GUI.DrawTexture(new Rect(0, 0, Screen.width / 20, Screen.height / 2), blue);
-		GUI.DrawTexture(new Rect(-Screen.width / 100, -Screen.height / 400, Screen.width / 20 + Screen.width / 50, Mathf.Min(Screen.height / 2, Screen.height / 2f / (float)threshold * ParticleNumber)), celeste);
+        GUI.DrawTexture(new Rect(-Screen.width / 100, -Screen.height / 400, Screen.width / 20 + Screen.width / 50, Mathf.Min(Screen.height / 2, Screen.height / 2f / (float)threshold * ParticleNumber)), celeste);
     }
 }
